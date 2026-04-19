beb47a7b232c9a5a57324e2641b6ddcbbd195702
Sana Aijaz <aijazsana1628@gmail.com>
2026-04-19 20:44:55 +0530

    Day 03 – Palindrome Number Solution
    LeetCode Problem: Palindrome Number
    Difficulty: Easy
    Approach: Reversed the number using modulo and division, then compared with original value.

---
+++ Day03/PalindromeNumber.java
@@ -0,0 +1,24 @@
+// Day 03 – Palindrome Number
+// LeetCode Problem: Palindrome Number
+// Difficulty: Easy
+// https://leetcode.com/problems/palindrome-number/
+
+class Solution {
+    public boolean isPalindrome(int x) {
+
+        if (x < 0) {
+            return false;
+        }
+
+        int original = x;
+        int rev = 0;
+
+        while (x > 0) {
+            int lastDigit = x % 10;
+            rev = rev * 10 + lastDigit;
+            x = x / 10;
+        }
+
+        return original == rev;
+    }
+}
