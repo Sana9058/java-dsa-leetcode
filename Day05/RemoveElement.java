8d88d3ee22738fb3bfa4c605e99a4a0457a7c918
Sana Aijaz <aijazsana1628@gmail.com>
2026-06-29 21:48:53 +0530

    Day 05 – Remove Element Solution
    LeetCode Problem: Remove Element
    Difficulty: Easy
    Approach: Used the two-pointer technique to overwrite valid elements in-place and return the new length with O(n) time and O(1) extra space.

---
+++ Day05/RemoveElement.java
@@ -0,0 +1,19 @@
+// Day 05 - Remove Element
+// LeetCode Problem: Remove Element
+// Difficulty: Easy
+// https://leetcode.com/problems/remove-element/
+
+class Solution {
+    public int removeElement(int[] nums, int val) {
+
+      int i = 0;
+
+      for(int j = 0; j < nums.length; j++){
+            if(nums[j] != val){
+                nums[i] = nums[j];
+                i++;
+            }
+        }
+        return i;
+    }
+}
